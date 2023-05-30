7c5e4c2bc3b833d1a55f88b8aee3b9411742318f
Akbarrych <thebestfriend152@gmail.com>
2023-05-30 13:10:21 +0600

    clean project

--- app/src/main/java/com/example/akbargit/MainActivity.kt
+++ app/src/main/java/com/example/akbargit/MainActivity.kt
@@ -4,17 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    // task2
-    // task1
-    // bdshjjs
-    // dgsgsgs
-    // sgjskbjk
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    // jssjgjsg
-    // task1
-    // sjhgjsglk
-    // bzfgahkosk
+
 }
